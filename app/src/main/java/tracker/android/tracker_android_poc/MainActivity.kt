aaa54349166beb6074ccec3802d1faa06e6f7284
Mohammed O. Tillawy <tillawy@gmail.com>
2018-10-03 13:16:22 +0300

    send the track command successfully

--- app/src/main/java/tracker/android/tracker_android_poc/MainActivity.kt
+++ app/src/main/java/tracker/android/tracker_android_poc/MainActivity.kt
@@ -1,6 +1,5 @@
 package tracker.android.tracker_android_poc
 
-import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -10,7 +9,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val i = Intent(applicationContext, TrackingService::class.java)
-        applicationContext.startService(i)
+        track(99,7878,"donatello", "domnn@renesance.com")
     }
+
+    fun track(userId: Int, clientId: Int, name: String, email: String) {
+        val i = Intent(this, TrackingService::class.java)
+        i.apply{
+            this.putExtra(USER_ID,3)
+            this.putExtra(CLIENT_ID,3)
+            this.putExtra(NAME,"Jonger")
+            this.putExtra(EMAIL,"Jonger@grendizer.com")
+        }
+        this.startService(i)
+    }
+
 }
--- app/src/main/java/tracker/android/tracker_android_poc/TrackingService.kt
+++ app/src/main/java/tracker/android/tracker_android_poc/TrackingService.kt
@@ -7,22 +7,69 @@ import io.socket.client.IO
 
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
+import org.json.JSONObject
+
+val USER_ID = "USER_ID"
+val CLIENT_ID = "CLIENT_ID"
+val NAME = "NAME"
+val EMAIL = "EMAIL"
+val TRACKING_ID = "TRACKING_ID"
+val APP_ID = "APP_ID"
 
 
 class TrackingService : Service() {
+
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
 //        return null
     }
 
+    val trackingId = ""
+
+    private var userId = 0
+    private var clientId = 0
+    private var name = ""
+    private var email = ""
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        userId = intent?.extras?.getInt(USER_ID) ?: 0
+        clientId = intent?.extras?.getInt(CLIENT_ID) ?: 0
+        name = intent?.extras?.getString(NAME) ?: ""
+        email = intent?.extras?.getString(EMAIL) ?: ""
+
+        connect(userId, clientId, name, email)
         return Service.START_NOT_STICKY
     }
 
+    data class TrackingCommand(
+            val userId: Int,
+            val clientId: Int,
+            val name: String,
+            val email: String
+    )
+
     var socket = IO.socket("http://macbook-air.duckdns.org:8282")
 
-    fun connect() {
+    fun connect(userId: Int, clientId: Int, name: String, email: String) {
+
+        val json = JSONObject()
+        json.put("user_id",userId)
+        json.put("client_id",clientId)
+        json.put("name",name)
+        json.put("email",email)
+        json.put("command","track")
+        json.put("os","Android")
+        val v = android.os.Build.VERSION.SDK_INT;
+        json.put("os_version","$v")
+        json.put("client_type","mobile")
+
+        json.put("device_brand",android.os.Build.BRAND)
+        json.put("device_model",android.os.Build.MODEL)
+        val android = 2
+        json.put("device_type",android)
+        json.put("app_id",2)
+
 
         val opts = IO.Options()
         opts.secure = true
@@ -30,9 +77,16 @@ class TrackingService : Service() {
         opts.reconnection = true
 
         socket.on(Socket.EVENT_CONNECT, Emitter.Listener {
-            println("connected")
-            socket.emit("foo", "hi")
-            socket.emit("asdf","324")
+            println("socket connected")
+            this.socket.emit("track", json.toString())
+        }).on(Socket.EVENT_ERROR, Emitter.Listener {
+            println("error:")
+            println(it)
+        })
+
+        socket.on("tracked", Emitter.Listener {
+            println("tracked")
+            println(it)
         })
         socket.connect()
     }
